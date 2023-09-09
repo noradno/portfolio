b757ec8caafd0e24fa466d5eec48580db8b21ce9
einartornes <45436606+einartornes@users.noreply.github.com>
2023-09-09 23:44:05 +0200

    add frame agreement info

--- disbursement.R
+++ disbursement.R
@@ -1,4 +1,6 @@
 # PTA disbursement level report
+# Two dataframes: 1) Agreement info, and 2) Disbursement info
+# The disbursement level report is supplemented by frame agreement info and disbursement
 
 library(readr)
 library(dplyr)
@@ -97,7 +99,7 @@ df_agreement_info <-
 
 
 
-# Include frame agreements in agreement_info
+# Include frame agreement info in agreement_info
 df_agreement_totals <-
   read_csv2(
     "output/agreement_totals.csv",
@@ -112,18 +114,18 @@ df_frameagreement_totals <- df_agreement_totals |>
   filter(agreement_no %in% vec_frameagreements)
 
 # Create column parent_agreement_no - here a duplicate of agreement_no
-df_frameagreement_totals <- df_frameagreement_totals |> 
+df_frameagreement_info <- df_frameagreement_totals |> 
   mutate(parent_agreement_no = agreement_no, .after = agreement_no)
 
 # Create column agreement_type - specify as frame agreements
 
-df_frameagreement_totals <- df_frameagreement_totals |> 
+df_frameagreement_info <- df_frameagreement_info |> 
   add_column(agreement_type = "Frame")
 
 # Add the frame agreements to the bottom of the df_agreement_info dataset.
 # By using the bind_rows any unmatched column names gives value NA
 
-df_agreement_info <- bind_rows(df_agreement_info, df_frameagreement_totals)
+df_agreement_info <- bind_rows(df_agreement_info, df_frameagreement_info)
 
 # Check to make sure there ar no duplicate agreement numbers
 df_agreement_info |> 
@@ -139,6 +141,9 @@ df_agreement_info |>
 # In this dataset an agreement number can have multiple records (across these non-static info)
 
 # Select columns and create grouped amount summaries across columns
+
+# I temporarily added parent_agreement_no and agreement_type
+
 df_agreement_disbursement <- df |>
   group_by(
     agreement_no,
@@ -148,17 +153,43 @@ df_agreement_disbursement <- df |>
     cost_center_name,
     programme_officer_name
   ) |>
-  summarise(amount = sum(amount))
-
-# Add frame agreements sums as separate rows, just like in statys_active
-# And as in statsys_aktive, not includeing a parent_agreement_number, but a column to identify parent agreements (and possibly sub-frame agreements).
-
-# We should start by creating a column to identify the subframe agreements
+  summarise(amount = sum(amount)) |> 
+  ungroup()
 
 
+df_frameagreement_disbursement <- df |>
+  filter(agreement_type == "Subunit") |>
+  group_by(
+    parent_agreement_no,
+    year
+  ) |>
+  summarise(amount = sum(amount)) |> 
+  ungroup()
+
+# Checks: one frameagreement is not present in the disbursement datset. Perhaps there are nothing disbursed.
+vec_test <- unique(df_frameagreement_disbursement$parent_agreement_no)
+
+df_agreement_disbursement |> 
+  filter(agreement_no %in% vec_frameagreements)
+
+# Create column parent_agreement_no - here a duplicate of agreement_no
+df_frameagreement_disbursement <- df_frameagreement_disbursement |> 
+  mutate(agreement_no = parent_agreement_no, .before = parent_agreement_no) |> 
+  select(-parent_agreement_no)
+
+# Add the frame agreements to the bottom of the df_agreement_disbursement dataset.
+# By using the bind_rows any unmatched column names gives value NA
+
+df_agreement_disbursement <- bind_rows(df_agreement_disbursement, df_frameagreement_disbursement)
 
 
 # Save dataframes ---------------------------------------------------------
 
-readr::write_csv2(df_agreement_info, "output/agreement_info.csv")
-readr::write_csv2(df_agreement_disbursement, "output/agreement_disbursement.csv")
+# Save datasets to prod folder
+path <- "C:/Users/u14339/UD Office 365 AD/Norad-Avd-Kunnskap - General/06. Porteføljestyring/P-Dash/prod/data/"
+
+readr::write_csv2(df_agreement_info,
+                  paste0(path, "agreement_info.csv"))
+
+readr::write_csv2(df_agreement_info,
+                  paste0(path, "agreement_disbursement.csv"))
